c8ff9ef1dbd0f528659d6b589859e8c60ef066ad
Sergio Hernández <shernandezj@sanitas.es>
2025-01-21 10:47:21 +0100

    quit deprecated things

--- bgp_backend/config/db.js
+++ bgp_backend/config/db.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/brawngpapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://127.0.0.1:27017/brawngpapp')
   .then(() => console.log('Conectado a MongoDB'))
   .catch((error) => console.error('Error al conectar a MongoDB', error));
